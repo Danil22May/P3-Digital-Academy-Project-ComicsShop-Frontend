4d6b084b72c69ed64ac6bbf5c35e3397e7a7c963
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-09-23 22:50:19 +0200

    responsive grid of cards semidone

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,11 @@ export default function Home() {
     { id: 4, name: "Spider-Man", price: 78 },
     { id: 5, name: "Spider-Man", price: 78 },
   ];
+
   return (
     <>
       <hr className="w-96 border-2 mx-auto my-10" />
-      <div className="grid grid-cols-2 md:m-10 gap-2 md:grid-cols-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 m-4 md:m-10">
         {products.map((product) => (
           <Card key={product.id} name={product.name} price={product.price} />
         ))}
