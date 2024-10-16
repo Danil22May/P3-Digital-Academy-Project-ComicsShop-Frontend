545161fb7b6f3b5015fb7601b7a8b060a68ccad7
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-10-16 02:29:28 +0200

    Login page done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,77 @@
 import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [status, changeStatus] = useState(1);
+  const [isRegistered, setIsRegestered] = useState(false);
+  const [registerError, setRegisterError] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [loggingError, setLoggingError] = useState(false);
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
   const audioRef = useRef(
     "https://us-tuna-sounds-files.voicemod.net/609379db-8438-4c15-bee3-0daa4b12a03b-1660380533038.mp3",
   );
   const playVoice = () => {
     audioRef.current.play();
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const handleRegister = () => {
+    if (loginData.password !== loginData.confirmPassword) {
+      setRegisterError(true);
+      return;
+    }
+
+    fetch("http://localhost:8080/api/v1/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: loginData.username,
+        password: loginData.password,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setIsRegestered(true);
+          navigate("/");
+        }
+      })
+      .catch(() => setRegisterError(true));
+  };
+
+  const handleLogin = () => {
+    fetch("http://localhost:8080/api/v1/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: loginData.username,
+        password: loginData.password,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          navigate("/");
+        } else {
+          setLoggingError(true);
+          setTimeout(() => setLoggingError(false), 5000);
+        }
+      })
+      .catch(() => setLoggingError(true));
+  };
+
   return (
     <div className="flex">
       <img
@@ -23,42 +87,66 @@ function Login() {
           {status ? (
             <>
               <h1 className="inter text-xl text-white/50">Login</h1>
-              <p className="text-white/40">Login</p>
+              <p className="text-white/40">Username</p>
               <input
                 type="text"
+                name="username"
+                value={loginData.username}
+                onChange={handleInputChange}
                 className="w-60 rounded border-2 border-white/40 bg-white/40 tracking-widest text-gray-900"
               />
               <p className="text-white/40">Password</p>
               <input
-                type="text"
+                type="password"
+                name="password"
+                value={loginData.password}
+                onChange={handleInputChange}
                 className="w-60 rounded border-2 border-white/40 bg-white/40 tracking-widest text-gray-900"
               />
               <button
-                onClick={playVoice}
+                onClick={handleLogin}
                 className="my-2 w-36 border-2 border-white/40 bg-white/25 p-2 font-thin tracking-wider text-white/70 transition-all hover:bg-white/10"
               >
                 Login
               </button>
+              {isLogged ? <p>Logged Successfully</p> : null}
+              {loggingError ? (
+                <p className="animate-pulse text-center font-bold text-red-900 transition-all">
+                  Wrong Credentials
+                </p>
+              ) : null}
             </>
           ) : (
             <>
               <h1 className="inter text-xl text-white/50">Register</h1>
-              <p className="text-white/40">Login</p>
+              <p className="text-white/40">Username</p>
               <input
                 type="text"
+                name="username"
+                value={loginData.username}
+                onChange={handleInputChange}
                 className="w-60 rounded border-2 border-white/40 bg-white/40 tracking-widest text-gray-900"
               />
               <p className="text-white/40">Password</p>
               <input
-                type="text"
+                type="password"
+                name="password"
+                value={loginData.password}
+                onChange={handleInputChange}
                 className="w-60 rounded border-2 border-white/40 bg-white/40 tracking-widest text-gray-900"
               />
-              <p className="text-white/40">Submit password</p>
+              <p className="text-white/40">Confirm password</p>
               <input
-                type="text"
+                type="password"
+                name="confirmPassword"
+                value={loginData.confirmPassword}
+                onChange={handleInputChange}
                 className="w-60 rounded border-2 border-white/40 bg-white/40 tracking-widest text-gray-900"
               />
-              <button className="my-2 w-36 border-2 border-white/40 bg-white/25 p-2 font-thin tracking-wider text-white/70 transition-all hover:bg-white/10">
+              <button
+                onClick={handleRegister}
+                className="my-2 w-36 border-2 border-white/40 bg-white/25 p-2 font-thin tracking-wider text-white/70 transition-all hover:bg-white/10"
+              >
                 Register
               </button>
             </>
@@ -67,7 +155,7 @@ function Login() {
             className="inter my-2 text-lg tracking-tighter text-orange-950 transition-all hover:scale-110"
             onClick={() => changeStatus((prev) => !prev)}
           >
-            {status ? <p>Not registered ?</p> : <p>Already registered ? </p>}
+            {status ? <p>Not registered?</p> : <p>Already registered?</p>}
           </button>
         </div>
       </div>
